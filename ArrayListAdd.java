e410831c69de1f853855805cde177fa5160f7423
Sanduni93 <nirodhajayaweera1993@gmail.com>
2019-01-22 00:23:12 +0530

    This about gow add elements to the ArrayList in java. Here also I have provided a sample output of my program.

---
+++ ArrayListAdd.java
@@ -0,0 +1,14 @@
+
+import java.util.ArrayList;
+class ArrayListAdd{
+public static void main(String[] args) {
+ArrayList<String> laps = new ArrayList<String>();
+laps.add("DELL");
+laps.add("Asuz");
+laps.add("Acer");
+laps.add("Lenovo");
+System.out.println(laps);
+	
+}
+
+}
